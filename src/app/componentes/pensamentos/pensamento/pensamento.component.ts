9adb605b196bbc11b4af7707a6d7d894912ed888
Antonio Alan <tonio.alan07@gmail.com>
2022-10-03 09:24:22 -0300

    criação do serviço HttpClient, criação do CRUD

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  @Input() pensamento : Pensamento= {
+  @Input() pensamento : Pensamento = {
     id: 0,
     conteudo: 'I love Angular',
     autoria: 'Alan',
